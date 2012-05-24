327184a06eebba76a0c910e9ab75d49a3e62db6f
Rich Jones <rich@anomos.info>
2012-05-23 22:49:16 -0700

    begin db

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = require('express').createServer()
   , express = require('express')
   , jqtpl = require("jqtpl");
 
+var cradle = require('cradle');
+var db = new(cradle.Connection)().database('notary');
+
 // Load the config file
 var config = require('config').Server;
 
@@ -17,3 +20,15 @@ app.get('/', function (req, res) {
   res.render(__dirname + '/html/index.html', {domain: config.siteDomain});
 });
 
+// DB Stuff
+
+db.exists(function (err, exists) {
+  if (err) {
+    console.log('error', err);
+  } else if (exists) {
+    console.log('Database exists..');
+  } else {
+    console.log('No database. Creating one..');
+    db.create();
+  }
+});
