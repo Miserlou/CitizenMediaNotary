a8abf9b271ad3ac95907ab216867dfbbb14efc49
Rich Jones <rich@anomos.info>
2012-06-06 16:10:56 -0700

    fix broken images

--- public/js/lightbox.js
+++ public/js/lightbox.js
@@ -48,8 +48,8 @@ lightbox = new Lightbox options
   LightboxOptions = (function() {
 
     function LightboxOptions() {
-      this.fileLoadingImage = 'images/loading.gif';
-      this.fileCloseImage = 'images/close.png';
+      this.fileLoadingImage = '/public/images/loading.gif';
+      this.fileCloseImage = '/public/images/close.png';
       this.resizeDuration = 700;
       this.fadeDuration = 500;
       this.labelImage = "Image";
